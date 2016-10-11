766c80d0738f9c226963f3572942fc4e77e4116e
Rickard Kamel <rickardkamel@gmail.com>
2016-10-12 00:15:07 +0200

    Added comments & components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,37 @@
+// --- AngularImports --- \\
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+
+// --- ProjectImports --- \\
+// SourceModules
 import { AppComponent } from './app.component';
-import { AboutComponent } from './about/about.component';
+import { ShellComponent } from './shell.component'
+
+// ComponentModules
 import { HomeModule } from './home/home.module';
+import { HeaderModule } from './header/header.module';
 import { AboutModule } from './about/about.module';
+
+// Routing
+import { AppRoutingModule } from './routes/app-routing.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShellComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HomeModule,
+    HeaderModule,
     AboutModule,
     HttpModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
