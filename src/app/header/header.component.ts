dd157f071959eced8c5d277788a8b3e2e8f82010
Rickard Kamel <rickardkamel@gmail.com>
2016-10-14 20:35:37 +0200

    fixed error from .scrollspy.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -13,15 +13,19 @@ export class HeaderComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.stickyHeader();
+  }
+  
+  stickyHeader(){
     (function () {
         $('.header').sticky({
             topSpacing: 0
         });
 
-        $('body').scrollspy({
-            target: '.navbar-custom',
-            offset: 70
-        })
+        // $('body').scrollspy({
+        //     target: '.navbar-custom',
+        //     offset: 70
+        // })
     }());
   }
 
