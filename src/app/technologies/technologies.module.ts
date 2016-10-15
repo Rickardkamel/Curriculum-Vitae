f417e9a6eda73e53067255f392c4b2fb8cb84e70
Rickard Kamel <rickardkamel@gmail.com>
2016-10-15 22:34:42 +0200

    initiated Technologies component.

---
+++ src/app/technologies/technologies.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+
+import { TechnologiesComponent } from './technologies.component';
+
+@NgModule({
+    imports: [],
+    exports: [
+        TechnologiesComponent
+    ],
+    declarations: [
+        TechnologiesComponent
+    ],
+    providers: []
+})
+
+export class TechnologiesModule {}
+    
