24c2e0bc75361684c3e8ef5e6a8e0db5ed020510
rickardkamel <rickardkamel@gmail.com>
2016-10-18 09:38:21 +0200

    added animated scrolling.

---
+++ src/app/app.js
@@ -0,0 +1,12 @@
+jQuery(function ($) {
+
+    (function () {
+        $('a[href*=#]').bind("click", function (e) {
+            var anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $(anchor.attr('href')).offset().top
+            }, 1000);
+            e.preventDefault();
+        });
+    } ());
+});
