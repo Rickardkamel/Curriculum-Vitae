b7c4b627a267ecd1b693f65fa3c1fdb4aa4802c7
Rickard Kamel <rickardkamel@gmail.com>
2016-10-18 22:33:43 +0200

    created app.js for general JS.

--- src/app/app.js
+++ src/app/app.js
@@ -1,5 +1,6 @@
 jQuery(function ($) {
 
+    // Animated scrolling
     (function () {
         $('a[href*=#]').bind("click", function (e) {
             var anchor = $(this);
@@ -9,4 +10,17 @@ jQuery(function ($) {
             e.preventDefault();
         });
     } ());
+
+    // Back to top
+    (function () {
+        $(window).scroll(function() {
+            alert('hello');
+            
+            if ($(this).scrollTop() > 500) {
+                $('.scroll-up').fadeIn();
+            } else {
+                $('.scroll-up').fadeOut();
+            }
+        });
+    }());
 });
