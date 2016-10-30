cf36e229e921dc84d383b61735bcb3e5dc86edb4
Rickard Kamel <rickardkamel@gmail.com>
2016-10-30 17:47:22 +0100

    modified ts to get data from service.

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,14 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { AboutService } from './about.service';
+import { IAbout } from './about.interface';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
 })
 export class AboutComponent implements OnInit {
-
-  constructor() { }
+  aboutMeInfo: IAbout;
+  constructor(private aboutService: AboutService) { }
 
   ngOnInit() {
+    this.aboutService.getAboutMe()
+      .subscribe(aboutData => {
+        this.aboutMeInfo = aboutData;
+      });
   }
-
 }
