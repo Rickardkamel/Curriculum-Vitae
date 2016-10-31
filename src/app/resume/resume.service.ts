61ebad02ac042508953dd48afa094b334ce4feb7
Rickard Kamel <rickardkamel@gmail.com>
2016-10-31 23:53:03 +0100

    Created and wrote code to get resumes from DB.

---
+++ src/app/resume/resume.service.ts
@@ -0,0 +1,23 @@
+import { GlobalVariableService } from '../shared/global-variable.service';
+import { Http, RequestOptions, Response, Headers } from '@angular/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/observable';
+import 'rxjs/Rx';
+
+@Injectable()
+export class ResumeService {
+    header = new Headers({
+        'Content-Type': 'application/json'
+    });
+    private apiUrl = this.globalVariable.apiUrl;
+
+    constructor(
+        private http: Http,
+        private globalVariable: GlobalVariableService
+    ) { }
+
+    getResumes() {
+        return this.http.get(this.apiUrl + 'api/resume', { headers: this.header })
+        .map((response: Response) => response.json());
+    }
+}
