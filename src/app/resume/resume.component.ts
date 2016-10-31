3080d7326711e38f5d88ddc8e525c614e35b6699
Rickard Kamel <rickardkamel@gmail.com>
2016-10-31 23:52:39 +0100

    added code to get resumes from service.

--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -1,14 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ResumeService } from './resume.service';
+import { IResume } from '../shared';
+
 @Component({
   selector: 'app-resume',
   templateUrl: './resume.component.html',
 })
 export class ResumeComponent implements OnInit {
 
-  constructor() { }
+  private resumeList: IResume[] = [];
+
+  constructor(private resumeService: ResumeService) { }
 
   ngOnInit() {
+    this.resumeService.getResumes()
+    .subscribe(resumes => {
+      this.resumeList = resumes;
+      console.log(this.resumeList);
+    });
   }
 
 }
