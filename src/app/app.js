15dc238d7f136e2db6fbc601cb2d82dad6ebc65d
Rickard Kamel <rickardkamel@gmail.com>
2016-10-31 23:51:12 +0100

    added JS to divide resume cards.

--- src/app/app.js
+++ src/app/app.js
@@ -13,22 +13,30 @@ jQuery(function ($) {
 
     // Back to top
     (function () {
-        $(window).scroll(function() {
+        $(window).scroll(function () {
             if ($(this).scrollTop() > 500) {
                 $('.scroll-up').fadeIn();
             } else {
                 $('.scroll-up').fadeOut();
             }
         });
-    }());
+    } ());
 
     // Fullscreen image
     (function () {
         $(".tt-fullHeight").height($(window).height());
 
-        $(window).resize(function(){
+        $(window).resize(function () {
             $(".tt-fullHeight").height($(window).height());
         });
 
-    }());
+    } ());
+
+    $(".resume").each(function () {
+        $('ul:even li').addClass('timeline-inverted');
+        $('ul:odd li').removeClass('timeline-inverted');
+        // $(this).find("ul").find("li").addClass("timeline-inverted")
+        //     .filter(":odd").removeClass("timeline-inverted").end()
+        //     .filter(":even").addClass("timeline-inverted");
+    });
 });
