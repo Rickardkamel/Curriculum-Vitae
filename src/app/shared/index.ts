54ea2da0abebe21c9b29830a04e8c151b2f33ae9
Rickard Kamel <rickardkamel@gmail.com>
2016-11-01 21:25:29 +0100

    added skill interface.

--- src/app/shared/index.ts
+++ src/app/shared/index.ts
@@ -2,3 +2,4 @@ export * from './about.interface';
 export * from './project.interface';
 export * from './resume.interface';
 export * from './timestamp.interface';
+export * from './skill.interface';
