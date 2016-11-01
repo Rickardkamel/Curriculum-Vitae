86dd3dfa9b5791e27a75c4afd34e7e87ec6e1818
Rickard Kamel <rickardkamel@gmail.com>
2016-11-01 21:25:51 +0100

    renamed Skills -> skill.

---
+++ src/app/skill/skill.js
@@ -0,0 +1,14 @@
+jQuery(function ($) {
+
+    'use strict';
+
+    $('.skill-progress').bind('inview', function (event, visible, visiblePartX, visiblePartY) {
+        if (visible) {
+            $.each($('div.progress-bar'), function () {
+                $(this).css('width', $(this).attr('aria-valuenow') + '%');
+            });
+            $(this).unbind('inview');
+        }
+    });
+
+});
