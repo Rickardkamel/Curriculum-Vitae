805a4daae9685bf72c47c9552b2d0db421269d72
Rickard Kamel <rickardkamel@gmail.com>
2016-11-05 20:55:31 +0100

    removed js.

--- src/app/app.js
+++ src/app/app.js
@@ -1,34 +1,59 @@
-jQuery(function ($) {
+// jQuery(function ($) {
 
-    // Animated scrolling
-    (function () {
-        $('a[href*=\\#]').bind("click", function (e) {
-            var anchor = $(this);
-            $('html, body').stop().animate({
-                scrollTop: $(anchor.attr('href')).offset().top
-            }, 1000);
-            e.preventDefault();
-        });
-    } ());
+//     // Animated scrolling
+//     (function () {
+//         $('a[href*=\\#]').bind("click", function (e) {
+//             var anchor = $(this);
+//             $('html, body').stop().animate({
+//                 scrollTop: $(anchor.attr('href')).offset().top
+//             }, 1000);
+//             e.preventDefault();
+//         });
+//     } ());
 
-    // Back to top
-    (function () {
-        $(window).scroll(function () {
-            if ($(this).scrollTop() > 500) {
-                $('.scroll-up').fadeIn();
-            } else {
-                $('.scroll-up').fadeOut();
-            }
-        });
-    } ());
+//     // Back to top
+//     (function () {
+//         $(window).scroll(function () {
+//             if ($(this).scrollTop() > 500) {
+//                 $('.scroll-up').fadeIn();
+//             } else {
+//                 $('.scroll-up').fadeOut();
+//             }
+//         });
+//     } ());
 
-    // Fullscreen image
-    (function () {
-        $(".tt-fullHeight").height($(window).height());
+//     // Fullscreen image
+//     (function () {
+//         $(".tt-fullHeight").height($(window).height());
 
-        $(window).resize(function () {
-            $(".tt-fullHeight").height($(window).height());
-        });
+//         $(window).resize(function () {
+//             $(".tt-fullHeight").height($(window).height());
+//         });
 
-    } ());
-});
+//     } ());
+
+//     $("#testimonial-carousel").owlCarousel({
+//         navigation: false,
+//         slideSpeed: 300,
+//         paginationSpeed: 400,
+//         responsiveRefreshRate: 200,
+//         responsiveBaseWidth: window,
+//         pagination: true,
+//         singleItem: true
+//     });
+
+
+//     $("#block-slider").owlCarousel({
+//         navigation: false,
+//         slideSpeed: 300,
+//         paginationSpeed: 400,
+//         responsiveRefreshRate: 200,
+//         responsiveBaseWidth: window,
+//         pagination: false,
+//         singleItem: true,
+//         navigation: true,
+//         navigationText: ["<span class='icon-left-open-big'></span>", "<span class='icon-right-open-big'></span>"]
+//     });
+
+
+// });
