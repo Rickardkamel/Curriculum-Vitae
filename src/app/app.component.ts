c7e2cfd43aaecb5a570ce00a94941cc99559077f
Rickard Kamel <rickardkamel@gmail.com>
2016-11-05 20:55:21 +0100

    added jquery to component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+declare var $: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
-export class AppComponent {
-  title = 'app works!';
+
+export class AppComponent implements OnInit {
+
+  ngOnInit() {
+    this.animatedScrolling();
+    this.backToTop();
+    this.fullScreenImage();
+  }
+
+  animatedScrolling() {
+    (function () {
+      $('a[href*=\\#]').bind('click', function (e) {
+        let anchor = $(this);
+        $('html, body').stop().animate({
+          scrollTop: $(anchor.attr('href')).offset().top
+        }, 1000);
+        e.preventDefault();
+      });
+    } ());
+  }
+
+  backToTop() {
+    (function () {
+      $(window).scroll(function () {
+        if ($(this).scrollTop() > 500) {
+          $('.scroll-up').fadeIn();
+        } else {
+          $('.scroll-up').fadeOut();
+        }
+      });
+    } ());
+  }
+
+  fullScreenImage() {
+    (function () {
+      $('.tt-fullHeight').height($(window).height());
+
+      $(window).resize(function () {
+        $('.tt-fullHeight').height($(window).height());
+      });
+    } ());
+  }
 }
