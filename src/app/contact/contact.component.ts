277d125d5f15edfe1702c9e37aa231c76391aad6
Rickard Kamel <rickardkamel@gmail.com>
2016-11-11 00:21:52 +0100

    added contact logic.

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,16 +1,39 @@
+import { ReactiveFormsModule } from '@angular/forms';
+import { ContactService } from './contact.service';
+import { IEmail } from '../shared/email.interface';
 import { Component, OnInit } from '@angular/core';
-declare var $: any;
+
+
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
 })
+
 export class ContactComponent implements OnInit {
 
-  constructor() { }
+  public message: IEmail = { name: '', email: '', message: '' };
 
-  ngOnInit() {
+  constructor(private contactService: ContactService) { }
+
+  ngOnInit() { }
+
+  onSubmit() {
+    this.contactService.postEmail(this.message)
+      .subscribe(response => this.handleResponse(response),
+      error => this.handleResponse(error)
+      );
   }
 
+  handleResponse(response) {
+    if (response.status === 'success') {
+      this.message = { name: '', email: '', message: '' };
+      alert('Thank you for your message');
+    }
+
+    if (response.status === 'error') {
+      alert('Sorry, your message could not be sent. Try again or send the email directly to rickardkamel@gmail.com. Thank you');
+    }
+  }
 }
 
 
