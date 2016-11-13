954d60908de29d8130f83d4fe89134d749478ac8
Rickard Kamel <rickardkamel@gmail.com>
2016-11-13 03:31:27 +0100

    added real apiUrl.

--- src/app/shared/global-variable.service.ts
+++ src/app/shared/global-variable.service.ts
@@ -2,5 +2,6 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class GlobalVariableService {
-    apiUrl: string = 'http://localhost:50149/';
+    // apiUrl: string = 'http://localhost:50149/';
+    apiUrl: string = 'http://api.rickardkamel.com/';
 }
